bb0ae0d0952540a2e6635eea03eecc44c27a0a0a
Nikolai Pismenkov <npismenkov@mail.ru>
2023-03-06 20:13:57 +0000

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,7 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 /**
- * The following block of code may be used to automatically register your
+ * The following block of code mayasdasdjnakjdnakjsdnkjasndjknasjkdnjkasdjkn be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
  * components and automatically register them with their "basename".
  *
